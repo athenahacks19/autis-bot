fca859eedea39814817da98f81bf0c0b4833148f
Diane Tobit <diane@acutulus.co>
2019-04-07 03:11:32 -0700

    remove welcome card

--- bots/dialogAndWelcomeBot.js
+++ bots/dialogAndWelcomeBot.js
@@ -9,15 +9,15 @@ class DialogAndWelcomeBot extends DialogBot {
     constructor(conversationState, userState, dialog, logger) {
         super(conversationState, userState, dialog, logger);
 
-        this.onMembersAdded(async context => {
-            const membersAdded = context.activity.membersAdded;
-            for (let cnt = 0; cnt < membersAdded.length; cnt++) {
-                if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                    const welcomeCard = CardFactory.adaptiveCard(WelcomeCard);
-                    await context.sendActivity({ attachments: [welcomeCard] });
-                }
-            }
-        });
+        // this.onMembersAdded(async context => {
+        //     const membersAdded = context.activity.membersAdded;
+        //     for (let cnt = 0; cnt < membersAdded.length; cnt++) {
+        //         if (membersAdded[cnt].id !== context.activity.recipient.id) {
+        //             const welcomeCard = CardFactory.adaptiveCard(WelcomeCard);
+        //             await context.sendActivity({ attachments: [welcomeCard] });
+        //         }
+        //     }
+        // });
     }
 }
 
